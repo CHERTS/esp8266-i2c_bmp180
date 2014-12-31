76821cd33f4d471cd6f365d91516d44aece115a0
Mikhail Grigorev <sleuthhound@gmail.com>
2014-12-31 13:17:49 +0500

    Added all project files
    Signed-off-by: Mikhail Grigorev <sleuthhound@gmail.com>

--- README.md
+++ README.md
@@ -1,4 +1,48 @@
-esp8266-i2c_bmp180
-==================
-
 ESP8266 - Driver for the temperature and pressure sensor BMP180
+===============================================================
+
+<b>Схема подключения BMP180 к ESP-01:</b><br>
+Вывод BMP180 GND подключен к GND ESP-01<br>
+Вывод BMP180 Vcc подключен к Vcc (3.3 Вольта) ESP-01<br>
+Вывод BMP180 SDA подключен к GPIO2 ESP-01<br>
+Вывод BMP180 SCL подключен к GPIO0 ESP-01 (смотрите файл i2c.h)<br>
+
+<b>Сборка под Windows:</b><br>
+1. <a href="http://programs74.ru/get.php?file=EspressifESP8266DevKitX86">Скачайте</a> и установите компилятор и SDK.<br>
+2. <a href="http://sourceforge.net/projects/mingw/files/Installer/">Скачайте</a> и установите MinGW. Запускаем mingw-get-setup.exe, в процессе установки выберите режим без GUI, то есть уберите галочку "...also install support for the graphical user interface".<br>
+3. <a href="http://programs74.ru/get.php?file=EspressifESP8266DevKitAddon">Скачайте</a> (84Mb) набор моих скриптов для автоматизации установки дополнительных модулей для MinGW.<br>
+4. Запустите из моего набора файл install-mingw-package.bat. Он установит основные модули для MinGW, установка должна пройти без ошибок.<br>
+5. Установите <a href="http://git-scm.com/download/win">Git for Windows</a> (после установки потребуется перезагрузить компьютер).<br>
+6. Запускаем консоль C:\MinGW\msys\1.0\msys.bat<br>
+7. В консоле выполните:<br>
+```
+cd /c/Espressif/examples
+git clone https://github.com/CHERTS/esp8266-i2c_bmp180
+cd esp8266-i2c_bmp180
+make
+make flash
+```
+
+--
+
+<b>For a single device, connect as follows BMP180 to ESP-01:</b><br>
+BMP180 GND to GND ESP-01<br>
+BMP180 Vcc to Vcc (3.3 Volts) ESP-01<br>
+BMP180 SDA to GPIO2 ESP-01<br>
+BMP180 SCL to GPIO0 ESP-01 (see file i2c.h)<br>
+
+<b>Building on Windows:</b><br>
+1. <a href="http://programs74.ru/get.php?file=EspressifESP8266DevKitX86">Download</a> and install compiler and SDK.<br>
+2. <a href="http://sourceforge.net/projects/mingw/files/Installer/">Download</a> and install MinGW. Run mingw-get-setup.exe, the installation process to select without GUI, ie uncheck "... also install support for the graphical user interface".<br>
+3. <a href="http://programs74.ru/get.php?file=EspressifESP8266DevKitAddon">Download</a> (84Mb) set my scripts to automate the installation of additional modules for MinGW.<br>
+4. Run the file from my set of install-mingw-package.bat. He will establish the basic modules for MinGW, installation should proceed without error.<br>
+5. Install <a href="http://git-scm.com/download/win">Git for Windows</a> (after installation to restart the computer).<br>
+6. Run the console C:\MinGW\msys\1.0\msys.bat<br>
+7. In the console, run:<br>
+```
+cd /c/Espressif/examples
+git clone https://github.com/CHERTS/esp8266-i2c_bmp180
+cd esp8266-i2c_bmp180
+make
+make flash
+```
---
+++ driver/i2c_bmp180.c
@@ -0,0 +1,201 @@
+/*
+    The driver for the temperature sensor and pressure BMP180
+    Official repository: https://github.com/CHERTS/esp8266-i2c_bmp180
+    Base on https://github.com/reaper7/esp8266_i2c_bmp180
+    This driver depends on the I2C driver https://github.com/zarya/esp8266_i2c_driver/
+
+    Copyright (C) 2014 reaper7
+    Copyright (C) 2014 Mikhail Grigorev (CHERTS)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#include <math.h>
+#include "driver/i2c.h"
+#include "driver/i2c_bmp180.h"
+
+int16_t ac1, ac2, ac3;
+uint16_t ac4, ac5, ac6;
+int16_t b1, b2;
+int16_t mb, mc, md; 
+
+int16_t ICACHE_FLASH_ATTR BMP180_readRegister16(uint8_t reg)
+{
+  i2c_start(); 
+  i2c_writeByte(BMP180_W);
+  if(!i2c_check_ack()){
+	#ifdef BMP180_DEBUG
+	  //os_printf("slave not ack..\n return \n");
+	  ets_uart_printf("BMP180_readRegister16: i2c_writeByte(0xEE) slave not ack..\r\nreturn\r\n");
+	#endif
+    i2c_stop();
+    return(0);
+  }
+  i2c_writeByte(reg);
+  if(!i2c_check_ack()){
+	#ifdef BMP180_DEBUG
+	  //os_printf("slave not ack..\n return \n");
+	  ets_uart_printf("BMP180_readRegister16: i2c_writeByte(reg) slave not ack..\r\nreturn\r\n");
+	#endif
+    i2c_stop();
+    return(0);
+  }
+  i2c_start(); 
+  i2c_writeByte(BMP180_R);
+  if(!i2c_check_ack()){
+	#ifdef BMP180_DEBUG
+	//os_printf("slave not ack..\n return \n");
+	ets_uart_printf("BMP180_readRegister16: i2c_writeByte(0xEF) slave not ack..\r\nreturn\r\n");
+	#endif
+    i2c_stop();
+    return(0);
+  }
+  uint8_t msb = i2c_readByte();                 
+  i2c_send_ack(1);                       		
+  uint8_t lsb = i2c_readByte();     
+  i2c_send_ack(0);                       		
+  i2c_stop();                           	
+  int16_t res = (msb << 8) + lsb;
+  return res;							
+}
+
+int16_t ICACHE_FLASH_ATTR BMP180_readRawValue(uint8_t cmd)
+{
+  i2c_start();                  			
+  i2c_writeByte(BMP180_W);
+  if(!i2c_check_ack()){
+	#ifdef BMP180_DEBUG
+	//os_printf("slave not ack..\n return \n");
+	ets_uart_printf("BMP180_readRawValue: i2c_writeByte(0xEE) slave not ack..\r\nreturn\r\n");
+	#endif
+    i2c_stop();
+    return(0);
+  }
+  i2c_writeByte(BMP180_CTRL_REG);	          	
+  if(!i2c_check_ack()){
+	#ifdef BMP180_DEBUG
+	  //os_printf("slave not ack..\n return \n");
+	  ets_uart_printf("BMP180_readRawValue: i2c_writeByte(0xF4) slave not ack..\r\nreturn\r\n");
+	#endif
+    i2c_stop();
+    return(0);
+  }
+  i2c_writeByte(cmd);       					
+  if(!i2c_check_ack()){
+	#ifdef BMP180_DEBUG
+	//os_printf("slave not ack..\n return \n");
+	ets_uart_printf("BMP180_readRawValue: i2c_writeByte(cmd) slave not ack..\r\nreturn\r\n");
+	#endif
+    i2c_stop();
+    return(0);
+  }
+  i2c_stop();                   			
+  os_delay_us(CONVERSION_TIME*1000);
+  int16_t res = BMP180_readRegister16(BMP180_DATA_REG);
+  return res;
+}
+
+uint32_t ICACHE_FLASH_ATTR BMP180_GetVal(uint8_t mode)
+{
+  int32_t UT;
+  uint16_t UP;
+  int32_t B3, B5, B6;
+  uint32_t B4, B7;
+  int32_t X1, X2, X3;
+  int32_t T, P;
+  
+  UT = BMP180_readRawValue(BMP_CMD_MEASURE_TEMP);
+  UP = BMP180_readRawValue(BMP_CMD_MEASURE_PRESSURE_0);
+
+  X1 = (UT - (int32_t)ac6) * ((int32_t)ac5) >> 15;
+  X2 = ((int32_t)mc << 11) / (X1 + (int32_t)md); 
+  B5 = X1 + X2;
+  T  = (B5+8) >> 4;
+
+  if(mode == GET_BMP_TEMPERATURE)
+    return (T);
+   
+  B6 = B5 - 4000;
+  X1 = ((int32_t)b2 * ((B6 * B6) >> 12)) >> 11;
+  X2 = ((int32_t)ac2 * B6) >> 11;
+  X3 = X1 + X2;
+  B3 = (((int32_t)ac1 * 4 + X3) + 2) / 4;
+  X1 = ((int32_t)ac3 * B6) >> 13;
+  X2 = ((int32_t)b1 * ((B6 * B6) >> 12)) >> 16;
+  X3 = ((X1 + X2) + 2) >> 2;
+  B4 = ((uint32_t)ac4 * (uint32_t)(X3 + 32768)) >> 15;
+  B7 = ((uint32_t)UP - B3) * (uint32_t)(50000UL);
+  
+  if (B7 < 0x80000000) {
+    P = (B7 * 2) / B4;
+  } else {
+    P = (B7 / B4) * 2;
+  }
+
+  X1 = (P >> 8) * (P >> 8);
+  X1 = (X1 * 3038) >> 16;
+  X2 = (-7357 * P) >> 16;
+  P = P + ((X1 + X2 + (int32_t)3791) >> 4);
+  
+  if(mode == GET_BMP_REAL_PRESSURE)
+    return P;
+    
+  if(mode >= GET_BMP_RELATIVE_PRESSURE)
+    return (int32_t)(pow(((float)MYALTITUDE/44330)+1,5.255F)*P);
+}
+
+bool ICACHE_FLASH_ATTR BMP180_Init()
+{
+	int16_t version = BMP180_readRegister16(BMP180_CHIP_ID_REG);
+	if (version != BMP180_CHIP_ID) {
+		#ifdef BMP180_DEBUG
+		char temp[80];
+		os_sprintf(temp, "BMP180: wanted chip id 0x%X, found chip id 0x%X\r\n",
+				  BMP180_CHIP_ID, version);
+		ets_uart_printf(temp);
+		#endif
+	    return 0;
+	}
+
+  if (!BMP180_readRegister16(BMP180_VERSION_REG))
+    return 0;
+
+  #ifdef BMP180_DEBUG
+  //os_printf("BMP180 read calibration data...\r\n");
+  ets_uart_printf("BMP180 read calibration data...\r\n");
+  #endif
+  ac1 = BMP180_readRegister16(0xAA);				 
+  ac2 = BMP180_readRegister16(0xAC);
+  ac3 = BMP180_readRegister16(0xAE);
+  ac4 = BMP180_readRegister16(0xB0);
+  ac5 = BMP180_readRegister16(0xB2);
+  ac6 = BMP180_readRegister16(0xB4);
+  b1 =  BMP180_readRegister16(0xB6);
+  b2 =  BMP180_readRegister16(0xB8);
+  mb =  BMP180_readRegister16(0xBA);
+  mc =  BMP180_readRegister16(0xBC);
+  md =  BMP180_readRegister16(0xBE);
+
+  #ifdef BMP180_DEBUG
+  ets_uart_printf("BMP180_Calibration:\r\n");
+  char temp[80];
+  os_sprintf(temp, "AC1: %ld, AC2: %ld, AC3: %ld, AC4: %ld, AC5: %ld, AC6: %ld, B1: %ld, B2: %ld, MB: %ld, MC: %ld, MD: %ld\r\n",
+		ac1, ac2, ac3, ac4, ac5, ac6, b1, b2, mb, mc, md);
+  ets_uart_printf(temp);
+  #endif
+  
+  return 1;
+}
+
